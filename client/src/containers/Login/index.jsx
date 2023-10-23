5d9b432a757c3f78449383393390eb4f3adf68b4
murali-madhunala <muralikrishna.madhunala@gmail.com>
2023-10-23 18:18:23 +0100

    feat: added login page

--- client/src/containers/Login/index.jsx
+++ client/src/containers/Login/index.jsx
@@ -1,18 +1,43 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 export const Login = () => {
   const [username, setUsername] = useState("");
+  const [isAuthenticated, setAuthenticated] = useState(false);
+  const navigate = useNavigate();
 
   const handleChange = (event) => {
     event.preventDefault();
     setUsername(event.target.value);
   };
+  const config = {
+    headers: {
+      "Content-Type": "application/json",
+    },
+  };
+  const url = "/api/generateToken";
+  const handleLogin = async () => {
+    const { data: token } = await axios.post(url, { username }, config);
+    localStorage.setItem("token", token.token);
+    setAuthenticated(true);
+  };
+
+  useEffect(() => {
+    const isAuthenticatedCheck = localStorage?.getItem("token")?.length > 1;
+    console.log(isAuthenticated, localStorage?.getItem("token"));
+
+    if (isAuthenticatedCheck) {
+      navigate("/dashboard");
+    }
+  }, [isAuthenticated, navigate]);
+
   return (
     <>
       <h1>Login Details</h1>
       <div>Enter your Username</div>
       <input type="text" value={username} onChange={handleChange} />
-      <button>LOGIN</button>
+      <button onClick={handleLogin}>LOGIN</button>
     </>
   );
 };
